7e1725cb6d3709b2181c688116633aa967a74f0c
Samuel Antenor <102992507+samuelantenor@users.noreply.github.com>
2025-02-04 15:25:48 -0500

    Update MainLayout.tsx

--- src/components/Layout/MainLayout.tsx
+++ src/components/Layout/MainLayout.tsx
@@ -1,17 +1,25 @@
 import { ReactNode } from 'react';
 import { SideNav } from './SideNav';
+import { useLocation } from 'react-router-dom';
+import { cn } from '../../lib/utils';
 
 interface MainLayoutProps {
   children: ReactNode;
 }
 
 export function MainLayout({ children }: MainLayoutProps) {
+  const location = useLocation();
+  const isFormView = location.pathname.startsWith('/forms/');
+
   return (
     <div className="min-h-screen bg-black">
-      <SideNav />
-      <main className="pl-[0px]">
+      {!isFormView && <SideNav />}
+      <main className={cn(
+        "transition-[padding] duration-300",
+        !isFormView && "md:pl-64"
+      )}>
         <div className="min-h-screen bg-black text-white">
-          <div className="mx-auto px-4 sm:px-6 lg:px-8 py-8">
+          <div className="mx-auto px-0 sm:px-0 lg:px-0 py-0">
             {children}
           </div>
         </div>
