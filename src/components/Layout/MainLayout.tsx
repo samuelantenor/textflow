3361f0a206859ca7d6ae07584aa467ebe367d8e1
Samuel Antenor <102992507+samuelantenor@users.noreply.github.com>
2025-02-04 15:00:11 -0500

    Update MainLayout.tsx

--- src/components/Layout/MainLayout.tsx
+++ src/components/Layout/MainLayout.tsx
@@ -9,7 +9,7 @@ export function MainLayout({ children }: MainLayoutProps) {
   return (
     <div className="min-h-screen bg-black">
       <SideNav />
-      <main className="pl-64">
+      <main className="pl-[0px]">
         <div className="min-h-screen bg-black text-white">
           <div className="mx-auto px-4 sm:px-6 lg:px-8 py-8">
             {children}
